44be7194e3e7bc4cb9ed0c6572917b7d0b99615a
kingsiao <kingsolomonsiao@gmail.com>
2023-06-09 13:02:55 +0800

    Implementation of docker and kubernetes

--- event-bus/index.js
+++ event-bus/index.js
@@ -24,7 +24,14 @@ app.post('/events', (req, res) => {
   axios
     .post('http://localhost:4003/events', event)
     .catch((err) => { console.log(err.message) })
-  
+  axios
+    .post('http://localhost:4006/events', event)
+    .catch((err) => { console.log(err.message) })
+  axios
+    .post('http://localhost:4007/events', event)
+    .catch((err) => { console.log(err.message) })
+
+
   res.send({status: 'OK'})
 })
 
